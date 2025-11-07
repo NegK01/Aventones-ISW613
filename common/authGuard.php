4b400744bd32452d164ad92939eeadaf792a8ad9
Neg <mauricio.or99@gmail.com>
2025-11-06 20:48:42 -0600

    fix(authGuard): se soluciona la redireccion de pagina para usuarios invitados

--- common/authGuard.php
+++ common/authGuard.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 // si no hay user_id, manda al index (este guardia solo se encuentra en las paginas privadas)
 if (empty($_SESSION['user_id'])) {
-    header('Location: /Proyecto-1/index.php');
+    header('Location: /Proyecto-1/pages/login.php');
     exit();
 } 
 
